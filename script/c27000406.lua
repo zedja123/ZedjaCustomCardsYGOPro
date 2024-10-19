aacb78b043563a5b3e530c764ea465879d0a09a9
zedja123 <miguel.zedja@hotmail.com>
2024-10-19 00:02:30 -0300

    Update c27000406.lua

--- script/c27000406.lua
+++ script/c27000406.lua
@@ -34,41 +34,5 @@ function c27000406.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
-	if tc:IsAttribute(ATTRIBUTE_WATER) then
-		-- WATER: Gain 1000 LP
-		Duel.Recover(tp,1000,REASON_EFFECT)
-	end
-	if tc:IsAttribute(ATTRIBUTE_EARTH) then
-		-- EARTH: Destroy 1 face-up card your opponent controls
-		local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,0,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g)
-			Duel.Destroy(g,REASON_EFFECT)
-		end
-	end
-	if tc:IsAttribute(ATTRIBUTE_WIND) then
-		-- WIND: Destroy 1 face-down card your opponent controls
-		local g=Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,0,LOCATION_ONFIELD,0,1,nil)
-		if #g>0 then
-			Duel.HintSelection(g)
-			Duel.Destroy(g,REASON_EFFECT)
-		end
-	end
-	if tc:IsAttribute(ATTRIBUTE_LIGHT) then
-		-- LIGHT: Attack directly
-		local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_DIRECT_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e2)
-	end
-	if tc:IsAttribute(ATTRIBUTE_DARK) then
-		-- DARK: Inflict piercing battle damage
-		local e3=Effect.CreateEffect(e:GetHandler())
-		e3:SetType(EFFECT_TYPE_SINGLE)
-		e3:SetCode(EFFECT_PIERCE)
-		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e3)
-	end
 end
 
