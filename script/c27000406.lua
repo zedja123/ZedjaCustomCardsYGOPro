9fb433099f24a8b3a66e0d96089112d8a9174421
zedja123 <miguel.zedja@hotmail.com>
2024-10-18 23:59:54 -0300

    Update c27000406.lua

--- script/c27000406.lua
+++ script/c27000406.lua
@@ -24,7 +24,6 @@ end
 
 function c27000406.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
 	-- Apply effects based on the monster's attributes
 	if tc:IsAttribute(ATTRIBUTE_FIRE) then
 		-- FIRE: Gain 500 ATK
@@ -70,7 +69,6 @@ function c27000406.activate(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetCode(EFFECT_PIERCE)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e3)
-	end
 	end
 end
 
